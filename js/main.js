2a146d5a7472766537ea0295b55838e435026170
Jairos <jairos@gmail.com>
2025-06-02 12:47:04 -0500

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,107 @@
+// PRODUCTOS
+const productos = [
+    // abrigos
+    {
+        id: "abrigo_01",
+        titulo:"abrigo 1",
+        imagen: "img/abrigos/01.jpeg",
+        categoria:{
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 15000,
+    },
+    {
+        id: "abrigo_02",
+        titulo:"abrigo 2",
+        imagen: "img/abrigos/02.jpeg",
+        categoria:{
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 25000,
+    },
+    {
+        id: "abrigo_03",
+        titulo:"abrigo 3",
+        imagen: "img/abrigos/03.jpeg",
+        categoria:{
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 35000,
+    },
+    {
+        id: "abrigo_04",
+        titulo:"abrigo 4",
+        imagen: "img/abrigos/04.jpeg",
+        categoria:{
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 45000,
+    },
+    {
+        id: "abrigo_05",
+        titulo:"abrigo 5",
+        imagen: "img/abrigos/abrigo_01.jpg",
+        categoria:{
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 55000,
+    },
+    // camisetas    
+    {
+        id: "camiseta_01",
+        titulo:"camiseta 1",
+        imagen: "img/camisetas/01.jpeg",
+        categoria:{
+            nombre: "camisetas",
+            id: "camisetas"
+        },
+        precio: 10000,
+    },
+    {
+        id: "camiseta_02",
+        titulo:"camiseta 2",
+        imagen: "img/camisetas/02.jpeg",
+        categoria:{
+            nombre: "camisetas",
+            id: "camisetas"
+        },
+        precio: 20000,
+    },
+    {
+        id: "camiseta_03",
+        titulo:"camiseta 3",
+        imagen: "img/camisetas/03.jpeg",
+        categoria:{
+            nombre: "camisetas",
+            id: "camisetas"
+        },
+        precio: 30000,
+    },
+    {
+        id: "camiseta_04",
+        titulo:"camiseta 4",
+        imagen: "img/camisetas/04.jpeg",
+        categoria:{
+            nombre: "camisetas",
+            id: "camisetas"
+        },
+        precio: 40000,
+    },
+    {
+        id: "camiseta_05",
+        titulo:"camiseta 5",
+        imagen: "img/camisetas/camiseta_01.jpg",
+        categoria:{
+            nombre: "camisetas",
+            id: "camisetas"
+        },
+        precio: 50000,
+    }
+    
+
+]
